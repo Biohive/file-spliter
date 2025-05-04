b31f91d83951e3b6bb310484a3e51c1d8c91f69f
Biohive <clemens.genius@gmail.com>
2025-05-04 18:14:31 +0000

    commit #3 'storage01'

--- split-file.sh
+++ split-file.sh
@@ -1,7 +1,7 @@
 # !/bin/bash
 # 
 # This script helps split files (CSV) into smaller chunks
-# Change test 2
+# Change test 3 (from STORAGE01)
 
 # $file_path = "/Tier1/Historical_data.csv"
 # Testing with an ISO file I could find at the time... (Temporary)
